29d0b06833c46229076690e61c4a545a75c30f1b
Joy <muirujoybackup@gmail.com>
2025-08-24 22:46:08 +0300

    added

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,6 +1,10 @@
 import ErrorBoundary from '@/components/ErrorBoundary';
 import type { AppProps } from "next/app";
+import * as Sentry from '@sentry/react';
 
+componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+  Sentry.captureException(error, { extra: errorInfo });
+}
 
 
 function MyApp({ Component, pageProps }: AppProps) {
